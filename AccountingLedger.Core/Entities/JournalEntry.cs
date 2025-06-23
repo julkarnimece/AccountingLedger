975794a9f06eeda649fb7a7b1c27558e793470f4
Julkar Nime <julkarnime09@gmail.com>
2025-06-23 20:36:28 +0600

    projects are created using clean architecture and entities created

---
+++ AccountingLedger.Core/Entities/JournalEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingLedger.Core.Entities
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; } = string.Empty;
+        public ICollection<JournalEntryLine> JournalEntryLines { get; set; } = new List<JournalEntryLine>();
+    }
+}
---
+++ AccountingLedger.Core/Entities/JournalEntryLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingLedger.Core.Entities
+{
+    public class JournalEntryLine
+    {
+        public int Id { get; set; }
+        public int JournalEntryId { get; set; }
+        public JournalEntry JournalEntry { get; set; } = default!;
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; } = default!;
+
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+    }
+}
